776d1105b4cd8cb9948a48201ae6b47373d6357d
YidiWuCE21 <yidi.wu99@gmail.com>
2022-01-17 13:26:12 -0500

    Update distorter_sandbox.py

--- distorter_sandbox.py
+++ distorter_sandbox.py
@@ -1,33 +1,24 @@
-import Distorter
 import os
-import numpy as np
+import distortions
 
+# Path
 path = os.path.join("data.csv")
-columns = ["T1", "T2", "T3"]
-distorter = Distorter.Distorter()
-distorter.gaussian_variance = 1
-distorter.read_data(path, columns)
-distorter.apply_distortion("gaussian", 3, 0.005, 0.001)
+
+# 1. Create the distorter
+distorter = distortions.Distorter()
+
+# 2. Read in the data and specify the columns
+distorter.read_data(path, ["T1"])
+
+# 3. Apply distortions
+#distorter.gaussian_noise(5, count=5)
+distorter.offset(5, anomaly_size=0.1, count=1)
+#distorter.zero()
+#distorter.pure_noise(23, 5)
+#distorter.point(5, 5)
+
+# 4. View the data
+distorter.view_chart("distorted")
+
+# 5. Output the data to a csv
 distorter.distorted_data.to_csv('distorted.csv')
-
-columns = distorter.columns
-window_size = 10
-
-all_x = []
-all_y = []
-
-for col in columns:
-	data = distorter.distorted_data[col]
-	x_vals = []
-	y_vals = []
-	for i in range(data.size):
-		endpoint = i + window_size
-		if endpoint >= data.size:
-			break
-		x_vals.append(list(data[i:endpoint]))
-		y_vals.append(data[endpoint])
-	all_x.append(x_vals)
-	all_y.append(y_vals)
-
-multiinput_x = np.array(all_x)
-print(multiinput_x)
