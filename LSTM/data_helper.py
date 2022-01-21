7a5b11a18011ca0bf562dab23d60959d780ffbe1
theadriennelee <alee823@gmail.com>
2022-01-21 15:16:45 -0500

    added checking for tp, tn, fp, fn and updating prediction every time

--- LSTM/data_helper.py
+++ LSTM/data_helper.py
@@ -1,29 +1,84 @@
 import numpy as np
 import pandas as pd
 
+skipVal = 11841
+
 def load_newtimeseries(filename):
-    series = pd.read_csv(filename, sep=',', header=None, index_col=None, squeeze=True, skiprows=11841)
-    # skiprows
+    """
+    Load testing data.
+
+    Args:
+        filename (string): Name of file that contains testing data
+    Returns:
+        timestamps (array[int]): Timestamps
+        data (array[int]): Testing data
+        flag_dictionary (dict[int]): Dictionary that contains T/F flags that
+            determine if datapoint is valid or not
+    """
+    
+    # Load last 20% of data - testing data
+    series = pd.read_csv(filename, 
+                         sep=',', 
+                         header=None, 
+                         index_col=None, 
+                         squeeze=True, 
+                         skiprows=skipVal)
+    
+    # Seperate the data into individual arrays
     timestamps = series.values[:, 0]
     data = series.values[:, 1]
     flag = series.values[:, 2]
+    
+    # Initialize dictionary
     flag_dictionary = {}
     for x in range(len(timestamps)):
         if flag[x] == True:
             flag_dictionary[timestamps[x]] = True
-    print("data read " + str(data))
     
-    return data, flag_dictionary
+    return timestamps, data, flag_dictionary
 
 def load_timeseries_with_filename(filename, params):
-    series = pd.read_csv(filename, sep=',', header=0, index_col=0, squeeze=True, skipfooter=11841)
+    """
+    Load training and validation data.
+
+    Args:
+        filename (string): Name of file that contains training and validation data
+        params (dict[string]): List of parameters to split the data
+    Returns:
+        load_timeseries() : Function that will split the data
+    """
+    
+    # Load first 80% of data
+    series = pd.read_csv(filename, 
+                         sep=',', 
+                         header=0, 
+                         index_col=0, 
+                         squeeze=True, 
+                         skipfooter=skipVal)
+
     data = series.values[:, 0]
 
     return load_timeseries(data, params)
 
 
 def load_timeseries(data, params):
-    """Load time series dataset"""
+    """
+    Load timeseries dataset.
+
+    Args:
+        data (arr[int]): Data set
+        params (dict[string]): List of parameters to split the data
+    Returns:
+        x_train (arr[int]): x training values normalized
+        y_tain (arr[int]): y training values normalized
+        x_valid (arr[int]): x validation values normalized
+        y_valid (arr[int]): y validation values normalized
+        x_valid_raw (arr[int]): x validation values raw
+        y_valid_raw (arr[int]): y validation values raw
+        last_raw (arr[int]): last window values raw
+        last (arr[int]): last window values normalized
+        data: list of data
+    """
 
     adjusted_window = params['window_size']+ 1
 
@@ -63,42 +118,21 @@ def load_timeseries(data, params):
     last = np.array(last)
     last = np.reshape(last, (last.shape[0], last.shape[1], 1))
 
-    return [x_train, y_train, x_valid, y_valid, x_valid_raw, y_valid_raw, last_raw, last]
+    return [x_train, y_train, x_valid, y_valid, x_valid_raw, y_valid_raw, last_raw, last, data]
 
 def normalize_windows(window_data):
-    """Normalize data"""
+    """
+    Normalize data
+
+    Args:
+        window_data (arr[int]): List of values to be normalized
+
+    Returns:
+        normalized_data (arr[int]): List of normalized data
+    """
 
     normalized_data = []
     for window in window_data:
         normalized_window = [((float(p) / float(window[0])) - 1) for p in window]
         normalized_data.append(normalized_window)
     return normalized_data
-
-def update_input_vales(data, params):
-    adjusted_window = params['window_size']+ 1
-
-    # Split data into windows
-    raw = []
-    for index in range(len(data) - adjusted_window):
-        raw.append(data[index: index + adjusted_window])
-
-    # Normalize data
-    result = normalize_windows(raw)
-
-    raw = np.array(raw)
-    result = np.array(result)
-
-    # Split the input dataset into train and test
-    split_ratio = round(params['train_test_split'] * result.shape[0])
-    train = result[:int(split_ratio), :]
-    np.random.shuffle(train)
-
-    # x_train and y_train, for training
-    x_train = train[:, :-1]
-    y_train = train[:, -1]
-
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    y_valid_raw = raw[int(split_ratio):, -1]
-
-    return [x_train, y_train, y_valid_raw]
-
--- LSTM/train_predict.py
+++ LSTM/train_predict.py
@@ -9,23 +9,41 @@ from csv import reader
 from sklearn.metrics import mean_squared_error
 from keras.models import load_model
 
+def forecast(x_valid_raw, y_valid_raw, predicted, model, last_window, 
+             last_window_raw):
+    """
 
-def forecast(x_test_raw, y_test_raw, predicted, model, last_window, last_window_raw):
+    Predicts the next timestamp
+
+    Args:
+        x_valid_raw (arr[int]): Raw x test values
+        y_valid_raw (arr[int]): Raw y test values
+        predicted (arr[int]): Predicted values during validation
+        model (keras.model): LSTM model
+        last_window (arr[int]): History of data normalized
+        last_window_raw (arr[int]): Raw history of data
+
+    Returns:
+        next_timestamp_raw (int): Predicted next timestamp
+        rms (int): RMS value
+
+    """
+    
     predicted_raw = []
     
-    for i in range(len(x_test_raw)):
-        predicted_val = (predicted[i] + 1) * x_test_raw[i][0]
+    for i in range(len(x_valid_raw)):
+        predicted_val = (predicted[i] + 1) * x_valid_raw[i][0]
         predicted_raw.append(predicted_val)
 
     # Plot graph: predicted VS actual
-    plt.figure()
-    plt.subplot(111)
-    plt.plot(predicted_raw, label='Predicted')
-    plt.plot(y_test_raw, label='Actual')	
-    plt.legend()
-    plt.show()
+    # plt.figure()
+    # plt.subplot(111)
+    # plt.plot(predicted_raw, label='Predicted')
+    # plt.plot(y_valid_raw, label='Actual')	
+    # plt.legend()
+    # plt.show()
 
-    rms = mean_squared_error(y_test_raw, predicted_raw, squared=False)
+    rms = mean_squared_error(y_valid_raw, predicted_raw, squared=False)
     print("rms " + str(rms))
     
     next_timestamp = build_model.predict_next_timestamp(model, last_window)
@@ -34,24 +52,74 @@ def forecast(x_test_raw, y_test_raw, predicted, model, last_window, last_window_
     
     return [next_timestamp_raw, rms]
 
-def check_abnormal_data(next_timestamp_raw, rms, current_data):
-    threshold = 1
+def check_abnormal_data(next_timestamp_raw, flag_dictionary, current_data, 
+                        timestamp, true_positive, true_negative, 
+                        false_positive, false_negative, valid_data):
+    """
+
+    Compare new data against the predicted timestamp.
+    Places the timestamp of the data in true_positive, true_negative, 
+        false_positive or false_negative.
+
+    Args:
+        next_timestamp_raw (int): Predicted next timestamp
+        flag_dictionary (dict[int]): Dictionary that contains T/F flags that
+            determine if datapoint is valid or not
+        current_data (int): Data to be checked
+        timestamp (int): Timestamp of the data to be checked
+        true_positive (arr[int]): List of true positive timestamps
+        true_negative (arr[int]): List of true negative timestamps
+        false_positive (arr[int]): List of false positive timestamps
+        false_negative (arr[int]): List of flase negative timestamps
+        valid_data (arr[int]): List of valid data
+
+    Returns:
+        valid_data (arr[int]): List of valid data
+        true_positive (arr[int]): List of true positive timestamps
+        true_negative (arr[int]): List of true negative timestamps
+        false_positive (arr[int]): List of false positive timestamps
+        false_negative (arr[int]): List of flase negative timestamps
+
+    """
+    
+    # Calculate thresholds
+    threshold = 0.02
     upper_limit = next_timestamp_raw * (1 + threshold)
     lower_limit = next_timestamp_raw * (1 - threshold)
-    # upper_limit = next_timestamp_raw + rms
-    # lower_limit = next_timestamp_raw - rms
+
     print('Next_time_stamp_raw ' + str(next_timestamp_raw))
     print('Upper limit ' + str(upper_limit))
     print('Lower limit ' + str(lower_limit))
 
-    valid_data = []
-    for data in current_data:
-        if float(data) > upper_limit or float(data) < lower_limit:
-            print("Abnormal data: " + str(data))
-        else:
-            valid_data.append(data)
+    # for i in range(len(current_data)):
+    #     if float(current_data[i]) > upper_limit or float(current_data[i]) < lower_limit:
+    #         if timestamps[i] in flag_dictionary:
+    #             true_negative.append(timestamps[i])
+    #         else:
+    #             false_negative.append(timestamps[i])
+    #             valid_data.append(current_data[i])
+    #     else:
+    #         if timestamps[i] in flag_dictionary:
+    #             false_positive.append(timestamps[i])
+    #         else:
+    #             true_positive.append(timestamps[i])
+    #             valid_data.append(current_data[i])
     
-    return valid_data
+    # Compare data
+    if float(current_data) > upper_limit or float(current_data) < lower_limit:
+        if timestamp in flag_dictionary:
+            true_negative.append(timestamp)
+        else:
+            false_negative.append(timestamp)
+            valid_data.append(current_data)
+    else:
+        if timestamp in flag_dictionary:
+            false_positive.append(timestamp)
+        else:
+            true_positive.append(timestamp)
+            valid_data.append(current_data)
+    
+    return valid_data, true_positive, true_negative, false_positive, false_negative
 
 def train_predict():
     """Train and predict time series data"""
@@ -63,16 +131,16 @@ def train_predict():
     # current_parameter_file = sys.argv[4]
     train_file = "./data/distorted.csv"
     parameter_file = "./training_config.json"
-    current_file = "./data/test1.csv"
-    current_parameter_file = "./training_config.json"
+    # current_file = "./data/test1.csv"
+    current_parameter_file = "./training_config_update.json"
 
     # Load training parameters
     params = json.loads(open(parameter_file).read())
     current_params = json.loads(open(current_parameter_file).read())
 
-    # Load time series dataset, and split it into train and test
-    x_train, y_train, x_test, y_test, x_test_raw, y_test_raw,\
-        last_window_raw, last_window = data_helper.load_timeseries_with_filename(train_file, params)
+    # Load time series dataset, and split it into train and validation
+    x_train, y_train, x_valid, y_valid, x_valid_raw, y_valid_raw,\
+        last_window_raw, last_window, data = data_helper.load_timeseries_with_filename(train_file, params)
 
 
     # Build RNN (LSTM) model
@@ -88,40 +156,102 @@ def train_predict():
         validation_split=params['validation_split'])
     model.save('initial_model.h5')
 
-    # Check the model against test set
-    predicted = build_model.predict_next_timestamp(model, x_test)        
+    # Check the model against validation set
+    predicted = build_model.predict_next_timestamp(model, x_valid)        
 
-    next_timestamp_raw, rms = forecast(x_test_raw, y_test_raw, predicted, model, last_window, last_window_raw)
+    # Predict the next timestamp
+    next_timestamp_raw, rms = forecast(x_valid_raw, 
+                                       y_valid_raw, 
+                                       predicted, 
+                                       model, 
+                                       last_window, 
+                                       last_window_raw)
 
-    current_data, flag_dictionary = data_helper.load_newtimeseries(train_file)
+    # Load testing time series dataset
+    timestamps, current_data, flag_dictionary = data_helper.load_newtimeseries(train_file)
 
+    # Update model every 50 timestamps
     update_frequency = 50
-    for i in range(0, current_data.size - 1, update_frequency):
-        updating_values = []
+    
+    true_positive = []
+    true_negative = []
+    false_positive = []
+    false_negative = []
+    
+    i = 0
+    
+    # For every timestamp, update the next predicted value
+    # With the updated predicted value, compare the value and the datapoint 
+        # and place it in tp, tn, fp or fn
+    # For every 50 timestamps, update the model with the new valid data
+    while (i + update_frequency) < current_data.size - 1:
+        valid_data = []
         for j in range(update_frequency):
-            updating_values.append(current_data[i + j]) 
-        print("updating values " + str(updating_values))
+            
+            # add new data point into the array of data
+            data = np.append(data, current_data[i + j])
+            
+            # Split data into train and validation set
+            x_train_update, y_train_update, x_valid_update, y_valid_update,\
+                x_valid_raw_update, y_valid_raw_update,\
+                last_window_raw_update, last_window_update, data = data_helper.load_timeseries(data, 
+                                                                                               params)
+            
+            # Check model against validation set
+            predicted = build_model.predict_next_timestamp(model, 
+                                                           x_valid_update)
+            
+            # Predict the next timestamp
+            next_timestamp_raw, rms = forecast(x_valid_raw_update, 
+                                               y_valid_raw_update, 
+                                               predicted, model, 
+                                               last_window_update,
+                                               last_window_raw_update)
+            
+            # Compare new data to predicted timestamp
+            valid_data, true_positive, true_negative, false_positive,\
+                false_negative = check_abnormal_data(next_timestamp_raw, 
+                                                     flag_dictionary, 
+                                                     current_data[i + j], 
+                                                     timestamps[i + j], 
+                                                     true_positive, 
+                                                     true_negative, 
+                                                     false_positive, 
+                                                     false_negative, 
+                                                     valid_data)
 
-        valid_data = check_abnormal_data(next_timestamp_raw, rms, updating_values)
+        # If there is enough data to update the model
+        # Load time series dataset, and split it into train and validation
+        if len(valid_data) > 10:
+            x_train_update, y_train_update, x_valid_update, y_valid_update, x_valid_raw_update, y_valid_raw_update,\
+                last_window_raw_update, last_window_update, data = data_helper.load_timeseries(valid_data, current_params)
 
-        # Load time series dataset, and split it into train and test
-        x_train_update, y_train_update, x_test_update, y_test_update, x_test_raw_update, y_test_raw_update,\
-            last_window_raw_update, last_window_update = data_helper.load_timeseries(valid_data, current_params)
-
-        model = load_model('initial_model.h5')
+            # Update model
+            model = load_model('initial_model.h5')
         
-        model.fit(
-            x_train_update,
-            y_train_update,
-            batch_size=params['batch_size'],
-            epochs=params['epochs'],
-            validation_split=params['validation_split'])
-        model.save('initial_model.h5')
-        predicted_update = build_model.predict_next_timestamp(model, x_test_update)
+            model.fit(
+                x_train_update,
+                y_train_update,
+                batch_size=params['batch_size'],
+                epochs=params['epochs'],
+                validation_split=params['validation_split'])
+            model.save('initial_model.h5')
+            predicted = build_model.predict_next_timestamp(model, x_valid_update)
 
-        print("predicted " + str(predicted_update))
-
-        next_timestamp_raw_updated, rms_updated = forecast(x_test_raw_update, y_test_raw_update, predicted_update, model, last_window_update, last_window_raw_update)        
+            print("predicted " + str(predicted))
+        
+        i = i + update_frequency
+    
+    # Export data
+    tp = pd.DataFrame(true_positive)
+    tp.to_csv('truePositive_002_50_75.csv')
+    tn = pd.DataFrame(true_negative)
+    tn.to_csv('trueNegative_002_50_75.csv')
+    fp = pd.DataFrame(false_positive)
+    fp.to_csv('falsePositive_002_50_75.csv')
+    fn = pd.DataFrame(false_negative)
+    fn.to_csv('falseNegative_002_50_75.csv')
+    
 
 
 
@@ -129,5 +259,6 @@ if __name__ == '__main__':
     # python3 train_predict.py ./data/sales.csv ./training_config.json
     # python train_predict.py ./data/test2.csv ./training_config.json ./data/test3.csv\
     #python train_predict.py ./data/test2.csv ./training_config.json ./data/test3.csv ./training_config_update.json 
+    #type_threshold_epoch_updateFreq
     print("hello")
     train_predict()
