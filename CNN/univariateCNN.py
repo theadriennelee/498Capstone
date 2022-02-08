645a70d0ae4df0f7e22612241e9f0ea6807a0e63
theadriennelee <alee823@gmail.com>
2022-02-07 23:46:11 -0500

    added testing

--- CNN/univariateCNN.py
+++ CNN/univariateCNN.py
@@ -13,6 +13,7 @@ from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
+from keras.models import load_model
 import pandas as pd
 import numpy as np
 import math
@@ -35,6 +36,7 @@ endVal = 19734
 midVal = 15787
 
 update_frequency = 50
+threshold = 0.02
 
 # split a univariate sequence into samples
 def split_sequence(sequence, n_steps):
@@ -88,6 +90,22 @@ def set_validation_data(index):
     Returns:
         boolean: Returns false if datapoint is a validation point
     """
+    if index >= midVal:
+        return False
+    elif index == 0:
+        return False
+    else:
+        return True
+
+def set_testing_data(index):
+    """
+    Helper function to read validation data
+
+    Args:
+        index (int): Index of datapoint
+    Returns:
+        boolean: Returns false if datapoint is a validation point
+    """
     if skipVal < index < midVal:
         return False
     elif index == 0:
@@ -95,24 +113,45 @@ def set_validation_data(index):
     else:
         return True
 
-def get_test_data(filename):
+def get_validation_data(filename):
     """
-    Load testing data
+    Load validation data
 
     Args:
         filename (string): Name of .csv file to load data from
     Returns:
         dataset (array): 2D array of testing data which includes timestamp and T1
     """
-    testset = pd.read_csv(filename, index_col=None, squeeze=True, skiprows=lambda x: set_validation_data(x))
+    series = pd.read_csv(filename, sep=',', header=0, index_col=0, usecols=['date', 'T1'], squeeze=True, skiprows=lambda x: set_validation_data(x))
+    # testset = pd.read_csv(filename, index_col=None, squeeze=True, skiprows=lambda x: set_validation_data(x))
     # for threshold testing T1 
-    testset = testset.drop(['Unnamed: 0','date','T1_class'],axis=1)
+    # testset = testset.drop(['Unnamed: 0','date','T1_class'],axis=1)
     # uncomment below to drop columns from Gaussian_T1.csv file 
     #testset = testset.drop(['Unnamed: 0','date','Appliances','lights','RH_1','T2','RH_2','T3','RH_3','T4','RH_4','T5','RH_5',
     #    'T6','RH_6','T7','RH_7','T8','RH_8','T9','RH_9','T_out','Press_mm_hg','RH_out','Windspeed','Visibility','Tdewpoint',
     #   'rv1','rv2','T1_class'],axis=1)
-    testset.to_csv('test_parsed.csv')
-    return(testset)
+    series.to_csv('test_parsed.csv')
+    return series
+
+def get_testing_data(filename):
+    """
+    Load validation data
+
+    Args:
+        filename (string): Name of .csv file to load data from
+    Returns:
+        dataset (array): 2D array of testing data which includes timestamp and T1
+    """
+    series = pd.read_csv(filename, sep=',', header=0, index_col=0, usecols=['date', 'T1'], squeeze=True, skiprows=lambda x: set_testing_data(x))
+    # testset = pd.read_csv(filename, index_col=None, squeeze=True, skiprows=lambda x: set_validation_data(x))
+    # for threshold testing T1 
+    # testset = testset.drop(['Unnamed: 0','date','T1_class'],axis=1)
+    # uncomment below to drop columns from Gaussian_T1.csv file 
+    #testset = testset.drop(['Unnamed: 0','date','Appliances','lights','RH_1','T2','RH_2','T3','RH_3','T4','RH_4','T5','RH_5',
+    #    'T6','RH_6','T7','RH_7','T8','RH_8','T9','RH_9','T_out','Press_mm_hg','RH_out','Windspeed','Visibility','Tdewpoint',
+    #   'rv1','rv2','T1_class'],axis=1)
+    series.to_csv('test_parsed.csv')
+    return series
         
 def load_flag_dictionary(filename):
     """
@@ -149,7 +188,7 @@ def load_flag_dictionary(filename):
 
 def check_abnormal_data(next_timestamp_raw, flag_dictionary, current_data, 
                         timestamp, true_positive, true_negative, 
-                        false_positive, false_negative, valid_data):
+                        false_positive, false_negative, valid_data, predicted_invalid, flags):
     """
     Compare new data against the predicted timestamp.
     Places the timestamp of the data in true_positive, true_negative, 
@@ -175,7 +214,6 @@ def check_abnormal_data(next_timestamp_raw, flag_dictionary, current_data,
     """
     
     # Calculate thresholds
-    threshold = 0.02
     upper_limit = next_timestamp_raw * (1 + threshold)
     lower_limit = next_timestamp_raw * (1 - threshold)
 
@@ -185,23 +223,66 @@ def check_abnormal_data(next_timestamp_raw, flag_dictionary, current_data,
     print('current data array', current_data[0])
     
     # Compare data
-    output_flag = None
     if float(current_data[0]) > upper_limit or float(current_data[0]) < lower_limit:
-        output_flag = False
+        flags.append(True)
+        predicted_invalid.append([timestamp, current_data[0]])
         if timestamp in flag_dictionary:
             true_negative.append(timestamp)
         else:
             false_negative.append(timestamp)
-            valid_data.append(current_data)
     else:
-        output_flag = True
+        flags.append(False)
         if timestamp in flag_dictionary:
             false_positive.append(timestamp)
         else:
             true_positive.append(timestamp)
-            valid_data.append(current_data)
+        valid_data.append(current_data)
     
-    return valid_data, true_positive, true_negative, false_positive, false_negative, output_flag 
+    return valid_data, predicted_invalid, flags, true_positive, true_negative, false_positive, false_negative
+
+def check_abnormal_test_data(next_timestamp_raw, current_data, 
+                        timestamp, predicted_invalid, flags, valid_data):
+    """
+    Compare new data against the predicted timestamp.
+    Places the timestamp of the data in true_positive, true_negative, 
+        false_positive or false_negative.
+    Args:
+        next_timestamp_raw (int): Predicted next timestamp
+        flag_dictionary (dict[int]): Dictionary that contains T/F flags that
+            determine if datapoint is valid or not
+        current_data (int): Data to be checked
+        timestamp (int): Timestamp of the data to be checked
+        true_positive (arr[int]): List of true positive timestamps
+        true_negative (arr[int]): List of true negative timestamps
+        false_positive (arr[int]): List of false positive timestamps
+        false_negative (arr[int]): List of flase negative timestamps
+        valid_data (arr[int]): List of valid data
+    Returns:
+        valid_data (arr[int]): List of valid data
+        true_positive (arr[int]): List of true positive timestamps
+        true_negative (arr[int]): List of true negative timestamps
+        false_positive (arr[int]): List of false positive timestamps
+        false_negative (arr[int]): List of flase negative timestamps
+        output_flag (boolean): Returns false if negative and true if positive
+    """
+    
+    # Calculate thresholds
+    upper_limit = next_timestamp_raw * (1 + threshold)
+    lower_limit = next_timestamp_raw * (1 - threshold)
+
+    print('Next_time_stamp_raw ' + str(next_timestamp_raw))
+    print('Upper limit ' + str(upper_limit))
+    print('Lower limit ' + str(lower_limit))
+    
+    # Compare data
+    if float(current_data[0]) > upper_limit or float(current_data[0]) < lower_limit:
+        flags.append(True)
+        predicted_invalid.append([timestamp, current_data[0]])
+    else:
+        flags.append(False)
+        valid_data.append(current_data)
+    
+    return predicted_invalid, flags, valid_data
 
 def calculate_mean_squared(predicted_val, actual_val):
     """
@@ -241,6 +322,172 @@ def predict_next_timestamp(model, raw_seq):
     yhat = model.predict(x_input, verbose=0)
     return yhat
 
+def validate(filename, raw_seq):
+
+    model = load_model('initial_model.h5')
+
+    validation_data = array(get_validation_data(filename))  
+    timestamps, flag_dictionary = load_flag_dictionary(filename)
+
+    true_positive = []
+    true_negative = []
+    false_positive = []
+    false_negative = []
+
+    mse_values = []
+
+    valid_data = [] 
+    predicted_invalid = []
+
+    flags = []
+    
+
+    #next step: continuously update the model to keep changing as it learns 
+    i = 0 
+    for data in validation_data:
+
+        # shape data
+        data = np.append(raw_seq, data)
+        X_update, y_update = split_sequence(data, n_steps)
+        X_update = X_update.reshape((X_update.shape[0], X_update.shape[1], n_features))
+
+        # predict next time stamp
+        yhat_update = predict_next_timestamp(model, raw_seq)
+
+        mse = calculate_mean_squared(yhat_update, data)
+        mse_values.append(mse)
+        
+        valid_data, predicted_invalid, flags, true_positive, true_negative, false_positive,\
+            false_negative = check_abnormal_data(yhat_update, 
+                                                    flag_dictionary, 
+                                                    data, 
+                                                    timestamps[i], 
+                                                    true_positive, 
+                                                    true_negative, 
+                                                    false_positive, 
+                                                    false_negative, 
+                                                    valid_data, predicted_invalid, flags)
+
+        # only update raw_seq is the data was found to be valid
+        raw_seq = data
+
+        # if there is enough data to update the model  
+        if len(valid_data) > update_frequency:
+            X, y = split_sequence(raw_seq, n_steps)
+            model.fit(X,y,epochs=5,verbose=1)
+            model.save('initial_model_validation.h5')
+            valid_data = []
+        i = i + 1
+
+    # Export data
+    tp = pd.DataFrame(true_positive)
+    tp.to_csv('truePositive.csv')
+    tn = pd.DataFrame(true_negative)
+    tn.to_csv('trueNegative.csv')
+    fp = pd.DataFrame(false_positive)
+    fp.to_csv('falsePositive.csv')
+    fn = pd.DataFrame(false_negative)
+    fn.to_csv('falseNegative.csv')
+    mseExport = pd.DataFrame(mse_values)
+    mseExport.to_csv('mseExport.csv')
+    pred_inv = pd.DataFrame(predicted_invalid)
+    pred_inv.to_csv("predicted_invalid.csv") 
+
+    data_export_vals = {
+        'Date': timestamps,
+        'T1': validation_data,
+        'T1_e': mse_values,
+        'T1_c': flags
+    }
+
+    data_export = pd.DataFrame(data_export_vals)
+    data_export.to_csv("T1_report.csv")
+
+    # Plot graph: Current Data vs MSE
+    plt.figure()
+    plt.subplot(111)
+    plt.plot(validation_data, label='Current Data')
+    plt.plot(mse_values, label='mse')	
+    plt.legend()
+    plt.show()
+
+    return raw_seq
+
+def test(filename, raw_seq):
+
+    model = load_model('initial_model_validation.h5')
+
+    testing_data = array(get_validation_data(filename))  
+    timestamps, flag_dictionary = load_flag_dictionary(filename)
+
+    mse_values = []
+
+    valid_data = [] 
+    predicted_invalid = []
+
+    flags = []
+    
+
+    #next step: continuously update the model to keep changing as it learns 
+    i = 0 
+    for data in testing_data:
+
+        # shape data
+        data = np.append(raw_seq, data)
+        X_update, y_update = split_sequence(data, n_steps)
+        X_update = X_update.reshape((X_update.shape[0], X_update.shape[1], n_features))
+
+        # predict next time stamp
+        yhat_update = predict_next_timestamp(model, raw_seq)
+
+        mse = calculate_mean_squared(yhat_update, data)
+        mse_values.append(mse)
+        
+        valid_data, predicted_invalid, flags, true_positive, true_negative, false_positive,\
+            false_negative = check_abnormal_test_data(yhat_update,  
+                                                    data, 
+                                                    timestamps[i], 
+                                                    predicted_invalid,
+                                                    flags,
+                                                    valid_data)
+
+        # only update raw_seq is the data was found to be valid
+        raw_seq = data
+
+        # if there is enough data to update the model  
+        if len(valid_data) > update_frequency:
+            X, y = split_sequence(raw_seq, n_steps)
+            model.fit(X,y,epochs=5,verbose=1)
+            model.save('initial_model_validation.h5')
+            valid_data = []
+        i = i + 1
+
+    # Export data
+    mseExport = pd.DataFrame(mse_values)
+    mseExport.to_csv('mseExport_test.csv')
+    pred_inv = pd.DataFrame(predicted_invalid)
+    pred_inv.to_csv("predicted_invalid_test.csv") 
+
+    data_export_vals = {
+        'Date': timestamps,
+        'T1': testing_data,
+        'T1_e': mse_values,
+        'T1_c': flags
+    }
+
+    data_export = pd.DataFrame(data_export_vals)
+    data_export.to_csv("T1_report.csv")
+
+    # Plot graph: Current Data vs MSE
+    plt.figure()
+    plt.subplot(111)
+    plt.plot(testing_data, label='Current Data')
+    plt.plot(mse_values, label='mse')	
+    plt.legend()
+    plt.show()
+
+    return
+
 
 def train_fit():
     """Train and predict time series data"""
@@ -248,8 +495,7 @@ def train_fit():
     # load data
     filename = "threshold_T1.csv"
     raw_seq = array(get_train_data(filename))
-    current_data = array(get_test_data(filename))
-    timestamps, flag_dictionary = load_flag_dictionary(filename)   
+    # current_data = array(get_test_data(filename)) 
 
     # split into samples
     X, y = split_sequence(raw_seq, n_steps)
@@ -275,72 +521,74 @@ def train_fit():
     model.save('initial_model.h5') 
 
     print("Prediction: ", yhat)
+
+    data = validate(filename, raw_seq)
     
-    true_positive = []
-    true_negative = []
-    false_positive = []
-    false_negative = []
+    # true_positive = []
+    # true_negative = []
+    # false_positive = []
+    # false_negative = []
 
-    mse_values = []
+    # mse_values = []
     
 
-    #next step: continuously update the model to keep changing as it learns 
-    i = 0 
-    while (i + update_frequency) < current_data.size -1:
-        valid_data = [] 
-        for j in range(update_frequency):
+    # #next step: continuously update the model to keep changing as it learns 
+    # i = 0 
+    # while (i + update_frequency) < current_data.size -1:
+    #     valid_data = [] 
+    #     for j in range(update_frequency):
 
-            # shape data
-            data = np.append(raw_seq, current_data[i + j])
-            X_update, y_update = split_sequence(data, n_steps)
-            X_update = X_update.reshape((X_update.shape[0], X_update.shape[1], n_features))
+    #         # shape data
+    #         data = np.append(raw_seq, current_data[i + j])
+    #         X_update, y_update = split_sequence(data, n_steps)
+    #         X_update = X_update.reshape((X_update.shape[0], X_update.shape[1], n_features))
 
-            # predict next time stamp
-            yhat_update = predict_next_timestamp(model, raw_seq)
+    #         # predict next time stamp
+    #         yhat_update = predict_next_timestamp(model, raw_seq)
 
-            mse = calculate_mean_squared(yhat_update, current_data[i + j])
-            mse_values.append(mse)
+    #         mse = calculate_mean_squared(yhat_update, current_data[i + j])
+    #         mse_values.append(mse)
             
-            valid_data, true_positive, true_negative, false_positive,\
-                false_negative, output_flag = check_abnormal_data(yhat, 
-                                                     flag_dictionary, 
-                                                     current_data[i + j], 
-                                                     timestamps[i + j], 
-                                                     true_positive, 
-                                                     true_negative, 
-                                                     false_positive, 
-                                                     false_negative, 
-                                                     valid_data)
+    #         valid_data, true_positive, true_negative, false_positive,\
+    #             false_negative, output_flag = check_abnormal_data(yhat, 
+    #                                                  flag_dictionary, 
+    #                                                  current_data[i + j], 
+    #                                                  timestamps[i + j], 
+    #                                                  true_positive, 
+    #                                                  true_negative, 
+    #                                                  false_positive, 
+    #                                                  false_negative, 
+    #                                                  valid_data)
 
-            # only update raw_seq is the data was found to be valid
-            if output_flag == True:
-                raw_seq = data
+    #         # only update raw_seq is the data was found to be valid
+    #         if output_flag == True:
+    #             raw_seq = data
 
-        # if there is enough data to update the model  
-        if len(valid_data) > 10:
-            X, y = split_sequence(raw_seq, n_steps)
-            model.fit(X,y,epochs=5,verbose=1)
-        i = i + update_frequency
+    #     # if there is enough data to update the model  
+    #     if len(valid_data) > 10:
+    #         X, y = split_sequence(raw_seq, n_steps)
+    #         model.fit(X,y,epochs=5,verbose=1)
+    #     i = i + update_frequency
 
-    # Export data
-    tp = pd.DataFrame(true_positive)
-    tp.to_csv('truePositive.csv')
-    tn = pd.DataFrame(true_negative)
-    tn.to_csv('trueNegative.csv')
-    fp = pd.DataFrame(false_positive)
-    fp.to_csv('falsePositive.csv')
-    fn = pd.DataFrame(false_negative)
-    fn.to_csv('falseNegative.csv')
-    mseExport = pd.DataFrame(mse_values)
-    mseExport.to_csv('mseExport.csv') 
+    # # Export data
+    # tp = pd.DataFrame(true_positive)
+    # tp.to_csv('truePositive.csv')
+    # tn = pd.DataFrame(true_negative)
+    # tn.to_csv('trueNegative.csv')
+    # fp = pd.DataFrame(false_positive)
+    # fp.to_csv('falsePositive.csv')
+    # fn = pd.DataFrame(false_negative)
+    # fn.to_csv('falseNegative.csv')
+    # mseExport = pd.DataFrame(mse_values)
+    # mseExport.to_csv('mseExport.csv') 
 
-    # Plot graph: Current Data vs MSE
-    plt.figure()
-    plt.subplot(111)
-    plt.plot(current_data, label='Current Data')
-    plt.plot(mse_values, label='mse')	
-    plt.legend()
-    plt.show()
+    # # Plot graph: Current Data vs MSE
+    # plt.figure()
+    # plt.subplot(111)
+    # plt.plot(current_data, label='Current Data')
+    # plt.plot(mse_values, label='mse')	
+    # plt.legend()
+    # plt.show()
                         
                 
 if __name__ == '__main__':
